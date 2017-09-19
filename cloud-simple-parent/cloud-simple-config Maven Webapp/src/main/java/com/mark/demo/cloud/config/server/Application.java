489015098955adf6176642017e0e0da5ae1db2a5
hxp <hxpwangyi@163.com>
2017-09-19 15:04:24 +0800

    111

---
+++ cloud-simple-parent/cloud-simple-config Maven Webapp/src/main/java/com/mark/demo/cloud/config/server/Application.java
@@ -0,0 +1,20 @@
+package com.mark.demo.cloud.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/*
+*hxp(hxpwangyi@126.com)
+*
+*
+*/
+@SpringBootApplication
+@EnableConfigServer
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
