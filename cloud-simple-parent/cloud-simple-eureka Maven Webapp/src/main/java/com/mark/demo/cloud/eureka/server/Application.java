053a2f1e471e7989ea0021b8af2071e8dc352840
hxp <hxpwangyi@163.com>
2017-09-19 15:23:18 +0800

    111

--- cloud-simple-parent/cloud-simple-eureka Maven Webapp/src/main/java/com/mark/demo/cloud/eureka/server/Application.java
+++ cloud-simple-parent/cloud-simple-eureka Maven Webapp/src/main/java/com/mark/demo/cloud/eureka/server/Application.java
@@ -1,19 +1,16 @@
-package com.mark.demo.cloud.config.server;
+package com.mark.demo.cloud.eureka.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /*
 *hxp(hxpwangyi@126.com)
-*
+*2017年9月19日
 *
 */
 @SpringBootApplication
-@EnableConfigServer
-@EnableDiscoveryClient
+@EnableEurekaServer
 public class Application {
 
 	public static void main(String[] args) {
