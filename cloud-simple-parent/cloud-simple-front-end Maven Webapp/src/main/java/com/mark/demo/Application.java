9fae85281a3f729b4c3ff57f5330d045c7b74bec
hxp <hxpwangyi@163.com>
2017-10-20 23:02:40 +0800

    11

--- cloud-simple-parent/cloud-simple-front-end Maven Webapp/src/main/java/com/mark/demo/Application.java
+++ cloud-simple-parent/cloud-simple-front-end Maven Webapp/src/main/java/com/mark/demo/Application.java
@@ -22,7 +22,7 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableDiscoveryClient
 @EnableFeignClients
 @EnableHystrix
-@EnableHystrixDashboard
+//@EnableHystrixDashboard
 public class Application {
 
 	public static void main(String[] args) {
