cde1c2fb3b7c484a7b4e781f542c5e6424b13c4a
hxp <hxpwangyi@163.com>
2017-10-21 21:18:17 +0800

    111

--- cloud-simple-parent/cloud-simple-eureka Maven Webapp/src/main/java/com/mark/demo/cloud/eureka/server/Application.java
+++ cloud-simple-parent/cloud-simple-eureka Maven Webapp/src/main/java/com/mark/demo/cloud/eureka/server/Application.java
@@ -1,8 +1,12 @@
 package com.mark.demo.cloud.eureka.server;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /*
 *hxp(hxpwangyi@126.com)
@@ -11,8 +15,16 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 */
 @SpringBootApplication
 @EnableEurekaServer
+@RestController
+@RefreshScope
 public class Application {
+	@Value("${name:World!}") String name ;
 
+    @RequestMapping("/hello")
+    public String home(){
+        return "Hello " + name;
+    }
+    
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
